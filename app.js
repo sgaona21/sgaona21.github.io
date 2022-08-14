37d0f53f8d21f55095918b94d83c1b3ec3f82e8b
sgaona21 <stevegaona21@gmail.com>
2022-08-14 01:51:56 -0700

    blue theme

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("js linked");
+
+const logo = document.getElementById("dev-logo");
+
+function makeAppear() {
+    logo.style.visibility = "visible"
+}
+
+function makeDisapear() {
+    logo.style.visibility = "hidden";
+    setTimeout(makeAppear, 1000);
+}
+
+
+setInterval(makeDisapear, 500);
+
+
