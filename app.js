da276aaa0b29b1b34aeb671af9113b78ef155844
sgaona21 <stevegaona21@gmail.com>
2022-08-14 22:42:18 -0700

    text animation

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 console.log("js linked");
 
-const logo = document.getElementById("dev-logo");
+
+// ***BLINKER ANIMATION***
+const logo = document.getElementById("blinker");
 
 function makeAppear() {
     logo.style.visibility = "visible"
@@ -8,10 +10,58 @@ function makeAppear() {
 
 function makeDisapear() {
     logo.style.visibility = "hidden";
-    setTimeout(makeAppear, 1000);
 }
 
+function makeBoth() {
+    setTimeout(makeDisapear, 0);
+    setTimeout(makeAppear, 500);
+}
 
-setInterval(makeDisapear, 500);
+setInterval(makeBoth, 1000);
+
+// *****TYPE ANIMATION*****
+
+const characters = document.getElementsByClassName("type-animation");
+
+// function biggerFunction(index, time) {
+//     function textsGoGreen(index) { 
+//         characters[index].style.color = "green";
+//     }
+//     function delayedGreen() {
+//         textsGoGreen(index);
+//     }
+//     setTimeout(delayedGreen, time);
+// }
+
+// function textAnimation() {
+//     time = 1000;
+//     for (i = 0; i < characters.length; i++) {
+//         time = time+=100;
+//         biggerFunction(i, time);
+//     }
+// }
+
+// textAnimation();
+
+function biggerFunction(index, time) {
+    function textsGoGreen(index) { 
+        characters[index].style.visibility = "visible";
+    }
+    function delayedGreen() {
+        textsGoGreen(index);
+    }
+    setTimeout(delayedGreen, time);
+}
+
+function textAnimation() {
+    time = 1000;
+    for (i = 0; i < characters.length; i++) {
+        time = time+=100;
+        biggerFunction(i, time);
+    }
+}
+
+textAnimation();
+
 
 
