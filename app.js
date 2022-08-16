37a2cf45e9048e32ab568632f98a12f1e6e7fd38
sgaona21 <stevegaona21@gmail.com>
2022-08-15 22:08:45 -0700

    svg icons

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 console.log("js linked");
 
+
+
+// *****BLINKER ANIMATION*****
+const blinker = document.getElementById("blinker");
+function makeAppear() {
+    blinker.style.visibility = "visible"
+}
+function makeDisappear() {
+    blinker.style.visibility = "hidden";
+}
+function makeBoth() {
+    setTimeout(makeDisappear, 0);
+    setTimeout(makeAppear, 500);
+}
+setInterval(makeBoth, 1000);
+
+
+
+
+
+// *****TYPE ANIMATION*****
 const title = document.getElementById("title");
-const title2String = title.innerHTML
+const title2String = title.innerHTML;
+function makeLetterAppear() {
+    title.innerHTML = "";
+    let time = 250
+    for (i = 0; i < title2String.length; i++) {
+        let letter = title2String[i];
+        function stringConnect() {
+            title.innerHTML = title.innerHTML + letter;
+        }
+        setTimeout(stringConnect, time);
+        time = time += 100;
+        console.log(title.innerHTML);
+    }  
+}
+makeLetterAppear();
 
-const randomLetter = title2String[22];
-console.log(title);
-console.log(title2String);
-console.log(randomLetter);
 
 
 
 
-// const title2Array = [...]
+
+
+
+
---
+++ spacetheme.js
@@ -0,0 +1,14 @@
+console.log("js linked");
+
+const title = document.getElementById("title");
+const title2String = title.innerHTML
+
+const randomLetter = title2String[22];
+console.log(title);
+console.log(title2String);
+console.log(randomLetter);
+
+
+
+
+// const title2Array = [...]
